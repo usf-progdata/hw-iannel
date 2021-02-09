42f1fa1a45d98cda95767d85a940d7cfd2b6a1b9
iannel <iannel@usf.edu>
2021-02-08 21:21:46 -0500

    HW2 Exercise 1

--- participation/data_wrangling.R
+++ participation/data_wrangling.R
@@ -1,2 +1,17 @@
 progdata::tutorial_wrangle()
 
+#(FILTER)
+##Looking back at the bfi_data, say I wanted to look at entries that have openness scores of greater than or equal to 3:
+
+openness_cut_off <-
+  bfi_data %>% 
+  filter(openness >= 3)
+
+openness_cut_off
+
+#filter()ing with multiple-criteria
+##Say I was only interested in looking at ages 20-29. HINT: the & operator is used to check if two logical operations are TRUE
+
+age_bfi <-
+  bfi_data %>% 
+  filter(age > 20 & age < 29)
